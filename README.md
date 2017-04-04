328edb9151b0ed773522315e82be9da32fc539f5
System Administrator <root@sgbcstudent1.hgen.slu.se>
2017-04-04 14:28:00 +0200

    Working version 1.1

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 custom wrapper for the ballgown tool for the eB3kit project
 
 
-This repo will contain two importants parts:
+This repository contains the following :
 + A wrapper for galaxy which will make ballgown work based on [this article](http://www.nature.com/nprot/journal/v11/n9/full/nprot.2016.095.html)
 + A tutorial on how to install and make a tool work under galaxy
++ A tutorial on how to make a wrapper for a custom tools
 
-Also this repo will contain on how to make your own custom wrapper for galaxy.
+
+### Changes and objectives
+- [x] Making the wrapper
+- [x] Making the wrapper function properly on galaxy
+- [ ] Cleaning up the code
+- [ ] Adding options
+- [ ] Tutorial on how to install to make a wrapper for a tool
+- [ ] Tutorial on how to add a custom tool to galaxy
--- ballgown/ballgown.R
+++ ballgown/ballgown.R
@@ -1,30 +1,53 @@
 #!/usr/bin/Rscript
 
-args = commandArgs(trailingOnly = TRUE)
-library(ballgown)
-library(genefilter)
+# Enabling commands line arguments
+# ----------------------------------------------------------------------------------------
 
-phendata = read.csv(args[1])
-#write.csv(phendata, file = args[2])
+args = commandArgs(trailingOnly = TRUE)
+
+# Loading required libraries. suppressMessages() remove all noisy attachement messages
+# ----------------------------------------------------------------------------------------
+
+suppressMessages(library(ballgown, warn.conflicts = FALSE))
+suppressMessages(library(genefilter, warn.conflicts = FALSE))
+suppressMessages(library(dplyr, warn.conflicts = FALSE))
+
+# Setup for the tool with some bases variables.
+# ----------------------------------------------------------------------------------------
+
 filtstr = 0.5
 pdat = 2
-out="results"
+phendata = read.csv(args[1])
+setwd(args[2])
+
+# Checking if the pdata file has the right samples names.
+# ----------------------------------------------------------------------------------------
+ 
+if (all(phendata$ids == list.files(".")) != TRUE)
+{
+  cat("Your phenotype data table does not match the samples names. ")
+}
+
 # Creation of the ballgown object based on data
 # ----------------------------------------------------------------------------------------
-bgi = ballgown(dataDir= "." , samplePattern="sample" ,pData = phendata)
-# #bgi = ballgown(dataDir = , samplePattern = , pData = phendata)
+bgi = ballgown(dataDir= "." , samplePattern="", pData = phendata, verbose = FALSE)
 
 # Filter the genes with an expression superior to the input filter
 # ----------------------------------------------------------------------------------------
 bgi_filt= subset(bgi, paste("rowVars(texpr(bgi)) >",filtstr), genomesubset = TRUE)
 
+# Creating the variables containing the transcripts and the genes and sorting them through the arrange() command.
+# ----------------------------------------------------------------------------------------
 
-r_transcript=stattest(bgi_filt,feature = "transcript", covariate = colnames(pData(bgi))[2], adjustvars = colnames(pData(bgi)[3]), getFC = TRUE, meas = "FPKM")
-r_genes=stattest(bgi_filt,feature = "gene", covariate = colnames(pData(bgi))[pdat], adjustvars = colnames(pData(bgi)[pdat+1]), getFC = TRUE, meas = "FPKM")
+results_transcripts=stattest(bgi_filt,feature = "transcript", covariate = colnames(pData(bgi))[pdat], adjustvars = colnames(pData(bgi)[pdat+1]), getFC = TRUE, meas = "FPKM")
+results_genes=stattest(bgi_filt,feature = "gene", covariate = colnames(pData(bgi))[pdat], adjustvars = colnames(pData(bgi)[pdat+1]), getFC = TRUE, meas = "FPKM")
 
 results_transcripts = data.frame(geneNames=ballgown::geneNames(bgi_filt), geneIDs=ballgown::geneIDs(bgi_filt), results_transcripts)
 results_transcripts = arrange(results_transcripts,pval)
 results_genes = arrange(results_genes,pval)
 
-write.csv(results_transcripts, paste(out,"_transcript.csv"), row.names = FALSE)
-write.csv(results_genes, paste(out,"_genes.scv"), row.names = FALSE)
+# Main output of the wrapper, two .csv files containing the genes and transcripts with their qvalue and pvalue
+# ----------------------------------------------------------------------------------------
+
+write.csv(results_transcripts, file=args[3], row.names = FALSE)
+write.csv(results_genes, file=args[4], row.names = FALSE)
